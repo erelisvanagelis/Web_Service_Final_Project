22180aee35187d707e532624c4e5e780ba752423
Antanas Tamašauskas <55793899+erelisvanagelis@users.noreply.github.com>
2021-05-31 19:45:22 +0300

    Pridėta Place klasė ir API metodai joms gauti bei tranformuoti iš JSON į objektą

--- src/main/java/lt/viko/eif/pyritefarmers/taxesapi/APIs/SkyScanner.java
+++ src/main/java/lt/viko/eif/pyritefarmers/taxesapi/APIs/SkyScanner.java
@@ -4,6 +4,7 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import lt.viko.eif.pyritefarmers.taxesapi.models.Options;
+import lt.viko.eif.pyritefarmers.taxesapi.models.Place;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -12,9 +13,11 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SkyScanner {
-    public static JSONObject getBrowseQuotesResult(Options options) throws IOException, ParseException {
+    public static JSONObject getBrowseQuotesService(Options options) throws IOException, ParseException {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0" +
@@ -36,5 +39,47 @@ public class SkyScanner {
         System.out.println(responseString);
 
         return jsonObject;
+    }
+
+    public static JSONObject getListPlacesService(String market, String query) throws IOException, ParseException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0" +
+                        "/" + market +
+                        "/EUR" +
+                        "/en-GB" +
+                        "/?query=" + query
+                        )
+                .get()
+                .addHeader("x-rapidapi-key", "c0dd1b869fmsh6b647aa42d660f4p1c85b7jsn5d757cf79a06")
+                .addHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseString = response.body().string();
+        JSONParser parse = new JSONParser();
+        JSONObject jsonObject = (JSONObject)parse.parse(responseString);
+        System.out.println(responseString);
+
+        return jsonObject;
+    }
+
+    public static List<Place> getPlaces(String market, String query) throws IOException, ParseException {
+        JSONObject jsonObject = getListPlacesService(market, query);
+        JSONArray jsonArray = (JSONArray) jsonObject.get("Places");
+
+        List<Place> places = new ArrayList<>();
+        for (int i = 0; i < jsonArray.size(); i++){
+            JSONObject placeJson = (JSONObject) jsonArray.get(i);
+            places.add(new Place(
+                    placeJson.get("PlaceId").toString(),
+                    placeJson.get("PlaceName").toString(),
+                    placeJson.get("CountryId").toString(),
+                    placeJson.get("RegionId").toString(),
+                    placeJson.get("CityId").toString(),
+                    placeJson.get("CountryName").toString()
+            ));
+        }
+        return places;
     }
 }
--- src/main/java/lt/viko/eif/pyritefarmers/taxesapi/testas.java
+++ src/main/java/lt/viko/eif/pyritefarmers/taxesapi/testas.java
@@ -2,12 +2,19 @@ package lt.viko.eif.pyritefarmers.taxesapi;
 
 import lt.viko.eif.pyritefarmers.taxesapi.APIs.SkyScanner;
 import lt.viko.eif.pyritefarmers.taxesapi.models.Options;
+import lt.viko.eif.pyritefarmers.taxesapi.models.Place;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.List;
 
 public class testas {
     public static void main(String[] args) throws IOException, ParseException {
-        SkyScanner.getBrowseQuotesResult(new Options(true));
+        SkyScanner.getBrowseQuotesService(new Options(true));
+        SkyScanner.getListPlacesService("LT", "Lithuania");
+        List<Place> places = SkyScanner.getPlaces("LT", "Lithuania");
+        for (Place place : places){
+            System.out.println(place);
+        }
     }
 }
